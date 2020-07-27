c770e5cb44b1d5fb293cc4669a4217a8aae1a125
bkkeerthana96 <68589444+bkkeerthana96@users.noreply.github.com>
2020-07-27 17:19:47 +0530

    Modularize functions to reduce  CCN

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,11 +1,19 @@
-
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
- 
+bool bpmAreOk(float bpm) {
   if(bpm < 70 || bpm > 150) {
     return false;
-  } else if(spo2 < 80) {
+  }
+ return true;
+}
+
+bool spoAreOk(float spo2){
+  if(spo2 < 80) {
     return false;
-  } else if(respRate < 30 || respRate > 60) {
+  } 
+ return true;
+}
+
+bool respRateOk(float respRate){
+  if(respRate < 30 || respRate > 60) {
     return false;
   }
   return true;
