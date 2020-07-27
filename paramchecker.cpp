cf8c0d005558326eeaeab5fa4dd2a62d796716d2
bkkeerthana96 <68589444+bkkeerthana96@users.noreply.github.com>
2020-07-27 17:44:41 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -5,14 +5,14 @@ bool bpmAreOk(float bpm) {
  return true;
 };
 
-bool spoAreOk(float spo2){
+namespace bool spoAreOk(float spo2){
   if(spo2 < 80) {
     return false;
   } 
  return true;
 };
 
-bool respRateOk(float respRate){
+namespace bool respRateOk(float respRate){
   if(respRate < 30 || respRate > 60) {
     return false;
   }
