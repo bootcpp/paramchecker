74b7d4077caf5e19ed712feeac2ebd6eaf648339
bkkeerthana96 <68589444+bkkeerthana96@users.noreply.github.com>
2020-07-28 15:57:34 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -6,7 +6,7 @@ bool vitalsAreOk(float bpm, float spo2, float respRate) {
   
   bool bpm_flag = vitalIsOk(70,150,bpm);
   bool spo2_flag =  vitalIsOk(80,100,spo2);
-  bool respRate_flag = VitalIsOk(30,60,respRate);
+  bool respRate_flag = vitalIsOk(30,60,respRate);
   
   return(bpm_flag && spo2_flag && respRate_flag);
 }
